6b2fd105b0db41b142b95a499042f4ead7b05223
Success Ahon <successahon@gmail.com>
2025-01-08 10:10:53 +0100

    Update [app].vue

--- pages/subapp/validate/[app].vue
+++ pages/subapp/validate/[app].vue
@@ -13,7 +13,9 @@ const auth = useAuthStore();
 
 onMounted(() => {
   if (auth.access_token) {
-    window.location.href = `${AppsObject[app]?.url}/auth/validate/${encodeURIComponent(auth.access_token)}`;
+   const encodededToken = encodeURIComponent(auth.access_token)
+    console.log("🚀 ~ onMounted ~ encodededToken:", encodededToken)
+    window.location.href = `${AppsObject[app]?.url}/auth/validate/app?token=${encodededToken}`;
   }
 });
 </script>
