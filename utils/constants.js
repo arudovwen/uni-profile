1daba361d01ea7abf18410fd72d56f2e8d24ddce
Success Ahon <successahon@gmail.com>
2025-02-10 14:29:49 +0100

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -320,7 +320,7 @@ export const Navigation = [
   {
     name: "Settings",
     url: "/",
-    key: "settings",
+    key: "/",
   },
 
   {
@@ -399,7 +399,7 @@ export const UserNavigation = [
   {
     name: "Settings",
     url: "/",
-    key: "settings",
+    key: "/",
   },
 
   {
@@ -427,7 +427,7 @@ export const OwnerNavigation = [
   {
     name: "Settings",
     url: "/",
-    key: "settings",
+    key: "/",
   },
   {
     name: "Business Information",
