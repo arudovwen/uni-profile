bac4d640bbbd432ac1ab4dd26e0727cbb541eff5
Success Ahon <successahon@gmail.com>
2025-02-11 11:26:55 +0100

    Update EmptyData.vue

--- components/EmptyData.vue
+++ components/EmptyData.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center p-4 h-[400px] text-gray-400 text-center"
+    class="flex flex-col items-center justify-center p-4 h-[280px] text-gray-400 text-center"
     :class="className"
   >
     <div class="mx-24 flex justify-center">
