dedcc790de6fde46c404cb26a48bbf28941b1270
Success Ahon <successahon@gmail.com>
2025-02-17 16:21:26 +0100

    Update

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -3,21 +3,19 @@
     v-if="appInfo?.logoUrl"
     :src="appInfo.logoUrl"
     :alt="appInfo.name"
-    class="w-auto h-7"
+    class="w-[150px] h-auto"
   />
-  <img
-    v-else
-    src="@/assets/apps/mat.png"
-    alt="sso logo"
-    class="w-auto h-10"
-  />
+  <img v-else src="@/assets/apps/mat.png" alt="sso logo" class="w-auto h-10" />
 </template>
 
 <script setup>
 const { app } = useRoute().params;
+const { appCode } = useRoute().query;
 const authStore = useAuthStore();
 
-const appInfo = computed(() => authStore.appList.find((i) => i.code === app));
+const appInfo = computed(() =>
+  authStore.appList.find((i) => i.code === app || i.code === appCode)
+);
 defineProps({
   className: {
     default: "w-auto h-8",
