18d8402b33e9c6582547ba4873aec5e156e22c6e
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-10-27 22:51:37 +0100

    Create external-form.vue

---
+++ pages/external-form.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="myFormContainer" style="height: 100vh;"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://tva.thetaskforge.co/inline.js'
+  script.dataset.width = '100%'
+  script.dataset.height = '100%'
+  script.dataset.requestId = '87aace11-1b59-41f6-8c61-5dcc0db3f69a'
+  script.dataset.targetId = 'myFormContainer'
+
+  document.getElementById('myFormContainer').appendChild(script)
+})
+</script>
